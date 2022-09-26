aedc596891fa50e87444fa948c7762cfaa89ce23
sahil <sahilphulwani.sp@gmail.com>
2022-09-27 00:30:55 +0530

    project done

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".server-button").addEventListener("click",function(){
+    document.querySelector(".main-txt").classList.add("clickact");
+});
+
+document.querySelector(".server-button").addEventListener("click",function(){
+    document.querySelector(".SignUp").classList.remove("clickact");
+});
